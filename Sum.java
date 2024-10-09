6fe3190aecd1df09346a5220ab31b238dfa64c28
Maximilian Ehlen <maximilian.ehlen@rewe-group.com>
2024-10-09 11:56:17 +0200

    A5

--- Sum.java
+++ Sum.java
@@ -1,5 +1,23 @@
 public class Sum {
     public static void main(String[] args) {
-        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+        System.out.println(
+                Integer.parseInt(args[0]) + Integer.parseInt(args[1])
+        );
+    }
+
+    static int product(int x, int y) {
+        return x * y;
+    }
+
+    static int squaresum(int x, int y) {
+        return (x * x) + (y * y);
+    }
+
+    static void output(String content){
+        System.out.println(content);
+    }
+
+    static void warning(){
+        System.out.println("WARNUNG");
     }
 }
